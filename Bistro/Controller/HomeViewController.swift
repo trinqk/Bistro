c05515c4d11b72a8b7fac2cc7ba625f69559c499
Josh <trinquele@gmail.com>
2018-03-20 01:05:56 -1000

    Finished initial setup

---
+++ Bistro/Controller/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Bistro
+//
+//  Created by Joshua  Lee on 3/19/18.
+//  Copyright © 2018 Joshua  Lee. All rights reserved.
+//
+
+import UIKit
+import CDYelpFusionKit
+
+class HomeViewController: UIViewController {
+
+    let yelpAPIClient = CDYelpAPIClient(apiKey: "rpx6A03BnzQqhi_CBdO6MNoByf__ufsoxpOz80n3pnbR2uFyRVZKT3JHfz8Y7bvltZ7-JBYJ6CWTHGJUXmtdtjqTwQUVQb7GjUaKaTu5aWcF5qE9NB-aTHtZTGWDWnYx")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
