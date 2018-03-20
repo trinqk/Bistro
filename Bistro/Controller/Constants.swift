66ead033857dcde2f61822aa10c45f53cd70869d
Josh <trinquele@gmail.com>
2018-03-20 01:29:24 -1000

    Setup

---
+++ Bistro/Controller/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Secret.swift
+//  Bistro
+//
+//  Created by Joshua  Lee on 3/20/18.
+//  Copyright © 2018 Joshua  Lee. All rights reserved.
+//
+
+import Foundation
+
+let yelpAPIKey = "rpx6A03BnzQqhi_CBdO6MNoByf__ufsoxpOz80n3pnbR2uFyRVZKT3JHfz8Y7bvltZ7-JBYJ6CWTHGJUXmtdtjqTwQUVQb7GjUaKaTu5aWcF5qE9NB-aTHtZTGWDWnYx"
--- Bistro/Controller/HomeViewController.swift
+++ Bistro/Controller/HomeViewController.swift
@@ -11,10 +11,12 @@ import CDYelpFusionKit
 
 class HomeViewController: UIViewController {
 
-    let yelpAPIClient = CDYelpAPIClient(apiKey: "rpx6A03BnzQqhi_CBdO6MNoByf__ufsoxpOz80n3pnbR2uFyRVZKT3JHfz8Y7bvltZ7-JBYJ6CWTHGJUXmtdtjqTwQUVQb7GjUaKaTu5aWcF5qE9NB-aTHtZTGWDWnYx")
+    let secret = yelpAPIKey
+    var yelpAPIClient: CDYelpAPIClient!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        yelpAPIClient = CDYelpAPIClient(apiKey: secret)
         
     }
     
